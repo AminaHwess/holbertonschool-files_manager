bf79b17315eecf48c499cd1b300c9f16d9ffa182
AminaHwess <amina.1111@yahoo.fr>
2025-06-17 09:58:34 +0100

    API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-import express from "express";
-import AppController from "../controllers/AppController";
+import AuthController from '../controllers/AuthController';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
 
-const router = express.Router();
+const express = require('express');
 
-router.get("/status", AppController.getStatus);
-router.get("/stats", AppController.getStats);
+const router = (app) => {
+  const paths = express.Router();
+  app.use(express.json());
+  app.use('/', paths);
+
+  paths.get('/status', ((req, resp) => AppController.getStatus(req, resp)));
+  paths.get('/stats', ((req, resp) => AppController.getStats(req, resp)));
+  paths.post('/users', ((req, resp) => UsersController.postNew(req, resp)));
+  paths.get('/connect', ((req, resp) => AuthController.getConnect(req, resp)));
+  paths.get('/disconnect', ((req, resp) => AuthController.getDisconnect(req, resp)));
+  paths.get('/users/me', ((req, resp) => UsersController.getMe(req, resp)));
+  paths.post('/files', ((req, resp) => FilesController.postUpload(req, resp)));
+  paths.get('/files/:id', ((req, resp) => FilesController.getShow(req, resp)));
+  paths.get('/files', ((req, resp) => FilesController.getIndex(req, resp)));
+  paths.put('/files/:id/publish', ((req, resp) => FilesController.putPublish(req, resp)));
+  paths.put('/files/:id/unpublish', ((req, resp) => FilesController.putUnpublish(req, resp)));
+  paths.get('/files/:id/data', ((req, resp) => FilesController.getFile(req, resp)));
+};
 
 export default router;
